c08b2b95408f98700e5ab467b11f2c11d9c565dc
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-10-29 14:03:52 +0300

    Completed

--- src/Clock.js
+++ src/Clock.js
@@ -3,11 +3,21 @@ import React, { useState, useEffect } from "react";
 function Clock() {
   const [time, setTime] = useState(new Date());
 
-  useEffect(() => {
+  /*  useEffect(() => {
     setInterval(() => {
       setTime(new Date());
     }, 1000);
-  }, []);
+  }, []); */
+
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
 
   return <div>{time.toString()}</div>;
 }
